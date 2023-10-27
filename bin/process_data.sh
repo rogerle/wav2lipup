22fb928996c5a08a8110d225c29fd3d4d6ccf653
roger <1050299330@qq.com>
2023-10-28 00:12:23 +0800

    数据处理脚本写好

--- bin/process_data.sh
+++ bin/process_data.sh
@@ -2,25 +2,25 @@
 
 TEMP=/tmp
 
-if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+if [[ ${stage} -le 0 ]] && [[ ${stop_stage} -ge 0 ]]; then
    python processData.py --data_root=../data \
                          --process_step=0 \
                          --gpu_num=1
 fi
 
-if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+if [[ ${stage} -le 1 ]] && [[ ${stop_stage} -ge 1 ]]; then
    python processData.py --data_root=../data \
                          --process_step=1 \
                          --gpu_num=1
 fi
 
-if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
+if [[ ${stage} -le 2 ]] && [[ ${stop_stage} -ge 2 ]]; then
    python processData.py --data_root=../data \
                          --process_step=2 \
                          --gpu_num=1
 fi
 
-if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
+if [[ ${stage} -le 3 ]] && [[ ${stop_stage} -ge 3 ]]; then
    python processData.py --data_root=../data \
                          --process_step=3 \
                          --gpu_num=1
