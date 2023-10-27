6f4672f1f94553f38690c92952a024060570fdc7
roger <1050299330@qq.com>
2023-10-28 00:26:25 +0800

    数据处理脚本写好

--- bin/process_data.sh
+++ bin/process_data.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
 TEMP=/tmp
+stage=0
+stop_stage=100
 
-if [[ ${stage} -le 0 ]] && [[ ${stop_stage} -ge 0 ]]; then
+source ./parse_options.sh || exit 1
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
    python processData.py --data_root=../data \
                          --process_step=0 \
-                         --gpu_num=1
+                         --gpu_num=1 || exit -1
 fi
 
-if [[ ${stage} -le 1 ]] && [[ ${stop_stage} -ge 1 ]]; then
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
    python processData.py --data_root=../data \
                          --process_step=1 \
-                         --gpu_num=1
+                         --gpu_num=1 || exit -1
 fi
 
-if [[ ${stage} -le 2 ]] && [[ ${stop_stage} -ge 2 ]]; then
+if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
    python processData.py --data_root=../data \
                          --process_step=2 \
-                         --gpu_num=1
+                         --gpu_num=1 || exit -1
 fi
 
-if [[ ${stage} -le 3 ]] && [[ ${stop_stage} -ge 3 ]]; then
+if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
    python processData.py --data_root=../data \
                          --process_step=3 \
-                         --gpu_num=1
+                         --gpu_num=1 || exit -1
 fi
