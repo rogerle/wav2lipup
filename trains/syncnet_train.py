69e31747f45353172db3726608f0d7bdfebac59c
roger <1050299330@qq.com>
2023-11-11 13:20:16 +0800

    syncnet训练代码调整

--- trains/syncnet_train.py
+++ trains/syncnet_train.py
@@ -15,9 +15,10 @@ import argparse
 from visualdl import LogWriter
 
 param = ParamsUtil()
+logloss = nn.BCELoss()
+
 
 def load_checkpoint(checkpoint_path, model, optimizer, reset_optimizer=False):
-
     print("load checkpoint from: {}".format(checkpoint_path))
     if torch.cuda.is_available():
         checkpoint = torch.load(checkpoint_path)
@@ -41,49 +42,46 @@ def load_checkpoint(checkpoint_path, model, optimizer, reset_optimizer=False):
 
 
 def save_checkpoint(model, optimizer, step, checkpoint_dir, epoch):
-
-    path = Path(checkpoint_dir+'/sync_checkpoint_step{:09d}.pth'.format(step))
+    path = Path(checkpoint_dir + '/sync_checkpoint_step{:09d}.pth'.format(step))
     optimizer_state = optimizer.state_dict() if param.save_optimizer_state else None
     torch.save({
-        "state_dict":model.state_dict(),
-        "optimizer":optimizer_state,
-        "global_step":step,
-        "global_epoch":epoch,
-    },path)
+        "state_dict": model.state_dict(),
+        "optimizer": optimizer_state,
+        "global_step": step,
+        "global_epoch": epoch,
+    }, path)
     print("save the checkpoint step {}".format(path))
 
 
-def eval_model(val_dataloader, global_step,device, model):
+def eval_model(val_dataloader, global_step, device, model):
     eval_steps = 1400
     losses = []
     print('Evaluating for {} steps'.format(eval_steps))
     with LogWriter(logdir="../logs/syncnet_train/eval") as writer:
         while 1:
-            for vstep,(x,mel,y) in enumerate(val_dataloader):
+            for vstep, (x, mel, y) in enumerate(val_dataloader):
                 model.eval()
 
                 x = x.to(device)
                 mel = mel.to(device)
                 y = y.to(device)
 
-                a,v = model(mel,x)
+                a, v = model(mel, x)
 
-                logloss = nn.BCELoss()
-                d = F.cosine_similarity(a,v)
-                loss = logloss(d.unsqueeze(1),y)
+                d = F.cosine_similarity(a, v)
+                loss = logloss(d.unsqueeze(1), y)
 
                 losses.append(loss.item())
 
                 if vstep > eval_steps: break
-            averaged_loss = sum(losses)/len(losses)
+            averaged_loss = sum(losses) / len(losses)
             writer.add_scalar(tag='eval/loss', step=global_step, value=averaged_loss)
             print('The evaluating loss:{}'.format(averaged_loss))
             return
 
 
-def train(device, model, train_dataloader, val_dataloader, optimizer, checkpoint_dir,start_step,start_epoch):
-
-    gloab_step = start_step
+def train(device, model, train_dataloader, val_dataloader, optimizer, checkpoint_dir, start_step, start_epoch):
+    global_step = start_step
     epoch = start_epoch
     numepochs = param.epochs
     checkpoint_interval = param.syncnet_checkpoint_interval
@@ -92,39 +90,39 @@ def train(device, model, train_dataloader, val_dataloader, optimizer, checkpoint
     with LogWriter(logdir="../logs/syncnet_train/train") as writer:
         while epoch < numepochs:
             running_loss = 0
-            prog_bar = tqdm(enumerate(train_dataloader),total=len(train_dataloader),leave = False)
+            prog_bar = tqdm(enumerate(train_dataloader), total=len(train_dataloader), leave=False)
 
-            for step,(x,mel,y) in prog_bar:
+            for step, (x, mel, y) in prog_bar:
                 model.train(),
                 optimizer.zero_grad()
 
-                #transform data to cuda
-                x= x.to(device)
+                # transform data to cuda
+                x = x.to(device)
                 mel = mel.to(device)
-                a,v = model(mel,x)
+                a, v = model(mel, x)
                 y = y.to(device)
 
-                #计算loss
-                logloss = nn.BCELoss()
-                d = F.cosine_similarity(a,v)
-                loss = logloss(d.unsqueeze(1),y)
+                # 计算loss
+                d = F.cosine_similarity(a, v)
+                loss = logloss(d.unsqueeze(1), y)
                 loss.backward()
 
                 optimizer.step()
 
-                gloab_step=gloab_step+1
+                global_step = global_step + 1
                 running_loss += loss.item()
-                if gloab_step % checkpoint_interval == 0:
-                    save_checkpoint(model,optimizer,gloab_step,checkpoint_dir,epoch)
+                if global_step % checkpoint_interval == 0:
+                    save_checkpoint(model, optimizer, global_step, checkpoint_dir, epoch)
 
-                if gloab_step % eval_interval == 0:
+                if global_step % eval_interval == 0:
                     with torch.no_grad():
-                        eval_model(val_dataloader,gloab_step,device,model)
+                        eval_model(val_dataloader, global_step, device, model)
 
-                prog_bar.set_description('Syncnet Train Epoch [{0}/{1}]'.format(epoch,numepochs))
-                prog_bar.set_postfix(train_loss=running_loss/(step+1),step=step + 1,gloab_step=gloab_step)
-                writer.add_scalar(tag='train/step_loss', step=gloab_step, value=running_loss/(step+1))
-            epoch +=1
+                prog_bar.set_description('Syncnet Train Epoch [{0}/{1}]'.format(epoch, numepochs))
+                prog_bar.set_postfix(train_loss=running_loss / (step + 1), step=step + 1, gloab_step=global_step)
+                writer.add_scalar(tag='train/step_loss', step=global_step, value=running_loss / (step + 1))
+            epoch += 1
+
 
 def main():
     args = parse_args()
@@ -138,9 +136,9 @@ def main():
     val_dataset = SyncNetDataset(args.data_root, run_type='eval', img_size=288)
 
     train_dataloader = DataLoader(train_dataset, batch_size=param.syncnet_batch_size, shuffle=True,
-                                  num_workers=param.num_works,drop_last=True)
+                                  num_workers=param.num_works, drop_last=True)
     val_dataloader = DataLoader(val_dataset, batch_size=8,
-                                num_workers=8,drop_last=True)
+                                num_workers=8, drop_last=True)
 
     if args.gpunum > 0:
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -155,12 +153,12 @@ def main():
     optimizer = optim.Adam([p for p in model.parameters() if p.requires_grad], lr=param.syncnet_learning_rate)
 
     start_step = 0
-    start_epoch =0
+    start_epoch = 0
 
     if checkpoint_path is not None:
         model, start_step, start_epoch = load_checkpoint(checkpoint_path, model, optimizer, reset_optimizer=False)
 
-    train(device,model,train_dataloader,val_dataloader,optimizer,checkpoint_dir,start_step,start_epoch)
+    train(device, model, train_dataloader, val_dataloader, optimizer, checkpoint_dir, start_step, start_epoch)
 
 
 def parse_args():
