29e3e54ea265989f40027e1180188308845e35af
roger <1050299330@qq.com>
2023-11-12 22:11:33 +0800

    discriminator网络结构加深调整

--- models/Discriminator.py
+++ models/Discriminator.py
@@ -27,17 +27,18 @@ class Discriminator(nn.Module):
                           BaseNormConv(256, 256, kernel_size=5, stride=1, padding=2)),
 
             nn.Sequential(BaseNormConv(256, 512, kernel_size=3, stride=2, padding=1),  # 5,5
+                          BaseNormConv(512, 512, kernel_size=3, stride=1, padding=1),
                           BaseNormConv(512, 512, kernel_size=3, stride=1, padding=1)),
 
             nn.Sequential(BaseNormConv(512, 512, kernel_size=3, stride=2, padding=1),  # 3,3
-                          BaseNormConv(512, 512, kernel_size=3, stride=1, padding=1), ),
+                          BaseNormConv(512, 512, kernel_size=3, stride=1, padding=1),
+                          BaseNormConv(512, 512, kernel_size=3, stride=1, padding=1)),
 
             nn.Sequential(BaseNormConv(512, 512, kernel_size=3, stride=1, padding=0),  # 1, 1
                           BaseNormConv(512, 512, kernel_size=1, stride=1, padding=0),
                           BaseNormConv(512, 512, kernel_size=1, stride=1, padding=0))])
 
-        self.binary_pred = nn.Sequential(nn.Conv2d(512, 512, kernel_size=1, stride=1, padding=0),
-                                         nn.Conv2d(512, 1, kernel_size=1, stride=1, padding=0),
+        self.binary_pred = nn.Sequential(nn.Conv2d(512, 1, kernel_size=1, stride=1, padding=0),
                                          nn.Sigmoid())
         self.label_noise = .0
 
