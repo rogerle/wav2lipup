0f2683b28037df700226be631397cfa2b412a1c1
roger <1050299330@qq.com>
2023-11-29 23:24:51 +0800

    多进程处理数据

--- bin/process_data.sh
+++ bin/process_data.sh
@@ -11,31 +11,26 @@ source ./parse_options.sh || exit 1
 
 if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
   CUDA_VISIBLE_DEVICES=${gpus} python processData.py --data_root=../data \
-                         --process_step=0 \
-                         --gpu_num=1 || exit -1
+                         --process_step=0  || exit -1
 fi
 
 if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
    CUDA_VISIBLE_DEVICES=${gpus} python processData.py --data_root=../data \
                          --preprocess_type=ASR \
-                         --process_step=1 \
-                         --gpu_num=1 || exit -1
+                         --process_step=1 || exit -1
 fi
 
 if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
    CUDA_VISIBLE_DEVICES=${gpus} python processData.py --data_root=../data \
-                         --process_step=2 \
-                         --gpu_num=1 || exit -1
+                         --process_step=2 || exit -1
 fi
 
 if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
    CUDA_VISIBLE_DEVICES=${gpus} python processData.py --data_root=../data \
-                         --process_step=3 \
-                         --gpu_num=1 || exit -1
+                         --process_step=3 || exit -1
 fi
 
 if [ ${stage} -le 4 ] && [ ${stop_stage} -ge 4 ]; then
    CUDA_VISIBLE_DEVICES=${gpus} python processData.py --data_root=../data \
-                         --process_step=4 \
-                         --gpu_num=1 || exit -1
+                         --process_step=4 || exit -1
 fi
