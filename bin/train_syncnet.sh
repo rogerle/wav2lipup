3cf63255aac25961abd0492da210aae15ed9035e
roger <1050299330@qq.com>
2023-12-16 13:03:49 +0800

    优化

--- bin/train_syncnet.sh
+++ bin/train_syncnet.sh
@@ -5,5 +5,5 @@ gpus=0
 CUDA_VISIBLE_DEVICES=${gpus} python ../trains/syncnet_train.py \
                              --data_root=../data/processed_data \
                              --checkpoint_dir=../data/syncnet_checkpoint \
-                             --config_file=../configs/train_config_288.yaml \
+                             --config_file=../configs/train_config.yaml \
                              --train_type=train
