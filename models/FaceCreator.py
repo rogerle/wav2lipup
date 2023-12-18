03f5be020cb087051f4771b048014cd3354a1441
乐军 <1050299330@qq.com>
2023-12-18 13:06:30 +0800

    把param这块做了修改，还有train里面的变量的调整

--- models/FaceCreator.py
+++ models/FaceCreator.py
@@ -105,7 +105,7 @@ class FaceCreator(nn.Module):
         ])
 
         self.output_block = nn.Sequential(BaseConv2D(80, 32, kernel_size=3, stride=1, padding=1),
-                                          BaseConv2D(32, 3, kernel_size=1, stride=1, padding=0),
+                                          nn.Conv2d(32, 3, kernel_size=1, stride=1, padding=0),
                                           nn.Sigmoid())
 
     def forward(self, audio_sequences, face_sequences):
