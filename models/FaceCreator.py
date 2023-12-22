0c05b92a2fe7a6c0592b7b1b090dcc939bdc7b63
乐军 <1050299330@qq.com>
2023-12-22 12:02:03 +0800

    修正优化

--- models/FaceCreator.py
+++ models/FaceCreator.py
@@ -15,49 +15,46 @@ class FaceCreator(nn.Module):
 
 
             nn.Sequential(BaseConv2D(16, 32, kernel_size=5, stride=2, padding=2,act='relu'), # 144 144
-                          BaseConv2D(32, 32, kernel_size=3, stride=1, padding=1, residual=True,act='relu')),
+                          nn.MaxPool2d(kernel_size=5,stride=1,padding=2)),
 
             nn.Sequential(BaseConv2D(32, 64, kernel_size=3, stride=2, padding=1,act='relu'),  # 72 72
-                          BaseConv2D(64, 64, kernel_size=3, stride=1, padding=1, residual=True,act='relu'),
-                          BaseConv2D(64, 64, kernel_size=3, stride=1, padding=1, residual=True,act='relu')),
+                          nn.MaxPool2d(kernel_size=3,stride=1,padding=1)),
 
-            nn.Sequential(BaseConv2D(64, 128, kernel_size=3, stride=2, padding=1,act='relu'),  # 转成 35 36
-                          BaseConv2D(128, 128, kernel_size=3, stride=1, padding=1, residual=True,act='relu'),
-                          BaseConv2D(128, 128, kernel_size=3, stride=1, padding=1, residual=True,act='relu'),
-                          BaseConv2D(128, 128, kernel_size=3, stride=1, padding=1, residual=True,act='relu')),
+            nn.Sequential(BaseConv2D(64, 128, kernel_size=3, stride=2, padding=1,act='relu'),  # 转成 36 36
+                          nn.MaxPool2d(kernel_size=3,stride=1,padding=1)),
 
             nn.Sequential(BaseConv2D(128, 256, kernel_size=3, stride=2, padding=1,act='relu'),  # 转成 18 18
-                          BaseConv2D(256, 256, kernel_size=3, stride=1, padding=1, residual=True,act='relu'),
-                          BaseConv2D(256, 256, kernel_size=3, stride=1, padding=1, residual=True,act='relu')),
+                          nn.MaxPool2d(kernel_size=3,stride=1,padding=1)),
 
-            nn.Sequential(BaseConv2D(256, 512, kernel_size=3, stride=2, padding=1,act='relu'),  # 9 9
-                          BaseConv2D(512, 512, kernel_size=3, stride=1, padding=1, residual=True,act='relu')),
+            nn.Sequential(BaseConv2D(256, 512, kernel_size=3, stride=2, padding=1,act='relu'),
+                          nn.MaxPool2d(kernel_size=3,stride=1,padding=1)), #9 9
 
-            nn.Sequential(BaseConv2D(512, 512, kernel_size=3, stride=2, padding=0,act='relu'),),  # 4 4
-            nn.Sequential(BaseConv2D(512, 512, kernel_size=3, stride=1, padding=0,act='relu')),  # 2 2
-            nn.Sequential(BaseConv2D(512, 512, kernel_size=2, stride=1, padding=0,act='relu'),) # 1 1
+            nn.Sequential(BaseConv2D(512, 512, kernel_size=3, stride=2, padding=0,act='relu'),
+                                    nn.MaxPool2d(kernel_size=1,stride=1,padding=0)),  # 4 4
+            nn.Sequential(BaseConv2D(512, 512, kernel_size=3, stride=1, padding=0,act='relu'),
+                                    nn.MaxPool2d(kernel_size=1,stride=1,padding=0)),  # 2 2
+            nn.Sequential(BaseConv2D(512, 512, kernel_size=2, stride=1, padding=0,act='relu'),
+                                    nn.MaxPool2d(kernel_size=1,stride=1,padding=0)) # 1 1
         ])
 
         self.audio_encoder = nn.Sequential(
             # [5,1,80,16]
             BaseConv2D(1, 32, kernel_size=3, stride=1, padding=1,act='relu'),
-            BaseConv2D(32, 32, kernel_size=3, stride=1, padding=1, residual=True,act='relu'),
-            BaseConv2D(32, 32, kernel_size=3, stride=1, padding=1, residual=True,act='relu'),
+            nn.MaxPool2d(kernel_size=3,stride=1,padding=1),
 
             BaseConv2D(32, 64, kernel_size=3, stride=(3, 1), padding=1,act='relu'),
-            BaseConv2D(64, 64, kernel_size=3, stride=1, padding=1, residual=True,act='relu'),
-            BaseConv2D(64, 64, kernel_size=3, stride=1, padding=1, residual=True,act='relu'),
+            nn.MaxPool2d(kernel_size=3,stride=1,padding=1),
 
             BaseConv2D(64, 128, kernel_size=3, stride=3, padding=1,act='relu'),
-            BaseConv2D(128, 128, kernel_size=3, stride=1, padding=1, residual=True,act='relu'),
-            BaseConv2D(128, 128, kernel_size=3, stride=1, padding=1, residual=True,act='relu'),
+            nn.MaxPool2d(kernel_size=3,stride=1,padding=1),
 
             BaseConv2D(128, 256, kernel_size=3, stride=(3, 2), padding=1,act='relu'),
-            BaseConv2D(256, 256, kernel_size=3, stride=1, padding=1, residual=True,act='relu'),
-            BaseConv2D(256, 256, kernel_size=3, stride=1, padding=1, residual=True,act='relu'),
+            nn.MaxPool2d(kernel_size=3,stride=1,padding=1),
 
             BaseConv2D(256, 512, kernel_size=3, stride=1, padding=0,act='relu'),
-            BaseConv2D(512, 512, kernel_size=1, stride=1, padding=0,act='relu')
+            nn.MaxPool2d(kernel_size=1, stride=1, padding=0),
+            BaseConv2D(512, 512, kernel_size=1, stride=1, padding=0,act='relu'),
+            nn.MaxPool2d(kernel_size=1, stride=1, padding=0)
         )
 
         self.face_decoder_block = nn.ModuleList([
@@ -93,7 +90,8 @@ class FaceCreator(nn.Module):
 
         self.output_block = nn.Sequential(BaseConv2D(80, 32, kernel_size=3, stride=1, padding=1,act='relu'),
                                           nn.Conv2d(32, 3, kernel_size=1, stride=1, padding=0),
-                                          nn.Sigmoid())
+                                          nn.Sigmoid(),
+                                          nn.MaxPool2d(kernel_size=1,stride=1,padding=0))
 
     def forward(self, audio_sequences, face_sequences):
 
